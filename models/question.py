7f7c35f18198dbfad53d9dc048445939a1f4e3d0
Shivani1537 <sj10@kent.ac.uk>
2026-03-25 21:46:34 +0000

    Created a Question class to show maths questions with text and answer. Added check_answer mathod to compare the correct answer with user input. Demonstrates use of OOP and supports core quiz functionality.

---
+++ models/question.py
@@ -0,0 +1,7 @@
+class Question:
+    def __init__(self, text, answer):
+         self.text = text
+         self.answer = answer
+
+    def check_answer(self, user_answer):
+         return user_answer == self.answer
