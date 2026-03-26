ccc6499b621ebc667a9a72a042de2b24bd79f2bc
Shivani1537 <sj10@kent.ac.uk>
2026-03-26 23:41:35 +0000

    Comment explaining adjusting for varying length of quiz.

--- SJ10-Mathbuddy-Assignment2/app.py
+++ SJ10-Mathbuddy-Assignment2/app.py
@@ -3,6 +3,9 @@ from math_engine import generate_random_question
 def main():
     score = 0
 
+# loop controls how many questions are asked in the quiz.
+# value inside range() determines total number of questions.
+# number can be adjusted to make quiz shorter or longer.
     for i in range(3):
         q = generate_random_question()
         print("Question:", q.text)
