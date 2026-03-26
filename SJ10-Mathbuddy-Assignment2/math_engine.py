edecce22d8c0a3a26055cc2230705e9bd36870e0
Shivani1537 <sj10@kent.ac.uk>
2026-03-26 17:28:44 +0000

    Function created to generate random addition questions using AdditionQuestion class. Begins implementation of dynamic question generation.

--- SJ10-Mathbuddy-Assignment2/math_engine.py
+++ SJ10-Mathbuddy-Assignment2/math_engine.py
@@ -1,3 +1,8 @@
 from models.question import AdditionQuestion, SubtractionQuestion, MultiplicationQuestion, DivisionQuestion
 
 import random
+
+def generate_addition_question():
+    a = random.randint(1, 10)
+    b = random.randint(1, 10)
+    return AdditionQuestion(a, b)
