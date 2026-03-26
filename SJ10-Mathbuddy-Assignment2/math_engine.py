1ce64f5f2ff39f573d6f1102a47f4f63c0744ae4
Shivani1537 <sj10@kent.ac.uk>
2026-03-26 17:42:39 +0000

    Added functions to generate subtraction, multiplication, and division questions. Creates variety of question and improves functionality of quiz.

--- SJ10-Mathbuddy-Assignment2/math_engine.py
+++ SJ10-Mathbuddy-Assignment2/math_engine.py
@@ -6,3 +6,18 @@ def generate_addition_question():
     a = random.randint(1, 10)
     b = random.randint(1, 10)
     return AdditionQuestion(a, b)
+
+def generate_subtraction_question():
+    a = random.randint(1, 10)
+    b = random.randint(1, 10)
+    return SubtractionQuestion(a, b)
+
+def generate_multiplication_question():
+    a = random.randint(1, 10)
+    b = random.randint(1, 10)
+    return MultiplicationQuestion(a, b)
+
+def generate_division_question():
+    answer = random.randint(1, 10)
+    divisor = random.randint(1, 10)
+    return DivisionQuestion(answer, divisor)
